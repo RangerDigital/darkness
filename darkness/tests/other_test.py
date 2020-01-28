d1723862c86f3d962bf17afd73c05d7a05fb65bb
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-01-28 17:46:28 +0100

    Add 404 test

---
+++ darkness/tests/other_test.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+
+
+def test_not_found(URL):
+    # Check if custom 404 is working.
+    response = requests.get(URL + "/hello")
+    json = response.json()
+
+    assert response.status_code == 404
+    assert response.headers["Content-Type"] == "application/json"
+    assert json["error"] == "Endpoint with that URL doesn't exist!"
