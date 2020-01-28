971a05194630d293dbf3fab084c38792164ee408
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-01-28 17:55:38 +0100

    Fix timing of tests

--- darkness/tests/other_test.py
+++ darkness/tests/other_test.py
@@ -10,5 +10,3 @@ def test_not_found(URL):
     assert response.status_code == 404
     assert response.headers["Content-Type"] == "application/json"
     assert json["error"] == "Endpoint with that URL doesn't exist!"
-
-# Other stuff.
